c4598ad6100d7f81be1a6a5775eeef9327c1798a
Ahmadjon Safarov <ahmadjonsafarov1112@gmail.com>
2022-12-23 23:26:21 +0500

    factory University

--- lib/university_model.dart
+++ lib/university_model.dart
@@ -6,6 +6,13 @@ class Universiyt{
   Universiyt({required this.alpha_two_code, required this.name, required this.web_pages, required this.country});
   factory Universiyt.fromJson(Map data){
     List<String> pages=[];
-
+    data['web_page'].forEach((element){
+      pages.add(element);
+    });
+    return Universiyt(
+        alpha_two_code: data["alpha_two_code"],
+        name: data['name'],
+        web_pages: pages,
+        country: data['country']);
   }
 }
